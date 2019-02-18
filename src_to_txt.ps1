80ed91c65dbd41532392c20370efe0798dca6083
Gideon <45065563+gideonza@users.noreply.github.com>
2019-02-18 11:54:34 +0200

    Add files via upload

---
+++ src_to_txt.ps1
@@ -0,0 +1,13 @@
+﻿<#
+.synopsis
+Author: Gideon Naude CoEx
+.Description
+This script will add a .txt extention to all .src file in a folder
+#>
+
+param (
+    [parameter(mandatory=$true)] [String] $path
+)
+
+Set-ExecutionPolicy RemoteSigned
+Get-ChildItem $path *.src | Rename-Item -NewName {$_.name + '.txt'}
